68573b1b7beb0ec34b1f8f6f9b07770e469b80b8
Camilo <camilo.mh2014@gmail.com>
2022-04-28 13:37:20 -0400

    Modifique ruta get

--- src/routes/medico.route.js
+++ src/routes/medico.route.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { getAllMedics, getMedicById, createMedic, updateMedic, deleteMedic } = require('../controllers/medico.controller')
+const { getAllMedics, getMedicoByRUT, createMedic, updateMedic, deleteMedic } = require('../controllers/medico.controller')
 const { validateFields } = require('../middleware/validateFields')
 const {validateMedico} = require('../middleware/validateMedico')
 
 router.get('/', getAllMedics)
-router.get('/:medicoId', getMedicById)
+router.get('/:rut', getMedicoByRUT)
 router.post('/', validateMedico, createMedic)
 router.put('/:medicoId', updateMedic)
 router.delete('/:medicoId', deleteMedic)
