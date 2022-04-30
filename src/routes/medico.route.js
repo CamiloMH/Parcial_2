cf1232cfa46b3fd6d3a0e2aa91b94160fc2a66ec
Camilo <camilo.mh2014@gmail.com>
2022-04-30 00:00:57 -0400

    add validation on router.put

--- src/routes/medico.route.js
+++ src/routes/medico.route.js
@@ -2,12 +2,12 @@ const express = require('express')
 const router = express.Router()
 const { getAllMedics, getMedicoByRUT, createMedic, updateMedic, deleteMedic } = require('../controllers/medico.controller')
 const { validateFields } = require('../middleware/validateFields')
-const {validateMedico} = require('../middleware/validateMedico')
+const {validateMedico} = require('../validations/validateMedico')
 
 router.get('/', getAllMedics)
 router.get('/:rut', getMedicoByRUT)
 router.post('/', validateMedico, createMedic)
-router.put('/:medicoId', updateMedic)
+router.put('/:medicoId', validateMedico, updateMedic)
 router.delete('/:medicoId', deleteMedic)
 
 
