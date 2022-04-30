38b7717c8ebbbe10995df2d298dcef3cd88acc8b
Camilo <camilo.mh2014@gmail.com>
2022-04-30 00:18:06 -0400

    remove comments

--- src/models/fichas.js
+++ src/models/fichas.js
@@ -2,14 +2,6 @@ const { DataTypes } = require('sequelize')
 const { sequelize } = require('../db/database')
 
 const Ficha = sequelize.define('Ficha', {
-  // medicoId: {
-  //   type: DataTypes.INTEGER(11),
-  //   allowNull: false
-  // },
-  // pacienteId: {
-  //   type: DataTypes.INTEGER(11),
-  //   allowNull: false
-  // },
   observaciones: DataTypes.TEXT('long'),
   createdDate: DataTypes.DATE,
   lastModifiedDate: DataTypes.DATE,
