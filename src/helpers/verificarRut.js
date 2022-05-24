e499b5b140708649a93ea1daf3bdfc261e722841
Camilo <camilo.mh2014@gmail.com>
2022-05-24 17:18:51 -0400

    add param dots

--- src/helpers/verificarRut.js
+++ src/helpers/verificarRut.js
@@ -2,7 +2,7 @@ const { validate, clean, format } = require('rut.js')
 
 const verificarRut = (rut) => {
   clean(rut)
-  const rutFormat = format(rut)
+  const rutFormat = format(rut, { dots: false })
   const rutValido = validate(rutFormat)
   return { rutFormat, rutValido }
 }
