37633e550ec7c56c176b8451097540436581e501
Camilo <camilo.mh2014@gmail.com>
2022-05-24 17:16:25 -0400

    add props paciente

--- src/models/pacientes.js
+++ src/models/pacientes.js
@@ -7,14 +7,30 @@ const Paciente = sequelize.define('Paciente', {
     allowNull: false
   },
   dv: {
-    type: DataTypes.STRING,
+    type: DataTypes.STRING(1),
     allowNull: false
   },
-  nombre: DataTypes.STRING(50),
-  edad: DataTypes.INTEGER(3),
+  nombre: {
+    type: DataTypes.STRING(50),
+    allowNull: false
+  },
+  apellidos: {
+    type: DataTypes.STRING(150),
+    allowNull: false
+  },
+  edad: {
+    type: DataTypes.STRING(3),
+    allowNull: false
+  },
+  correo: {
+    type: DataTypes.STRING(150),
+    allowNull: false
+  },
+  celular: {
+    type: DataTypes.INTEGER(9),
+    allowNull: false
+  },
   isDisabled: DataTypes.TINYINT,
-  correo: DataTypes.STRING(50),
-  celular: DataTypes.INTEGER(9),
   createdDate: DataTypes.DATE,
   lastModifiedDate: DataTypes.DATE
 }, {
