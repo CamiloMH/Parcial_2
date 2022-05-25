7121782606ac370699eb09670d11548608696c83
Camilo <camilo.mh2014@gmail.com>
2022-05-24 23:33:09 -0400

    delete validations medico

--- src/routes/medicos.js
+++ src/routes/medicos.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const { getAllMedics, getMedicoByRUT, getMedicoById, createMedic, updateMedic, deleteMedic, disabledMedic } = require('../controllers/medicos.controller')
-const { validateFields } = require('../middleware/validateFields')
-const { validateMedico } = require('../validations/validateMedico')
 
 router.get('/', getAllMedics)
 router.get('/:medicoId', getMedicoById)
 router.get('/rut/:rut', getMedicoByRUT)
-router.post('/', validateMedico, createMedic)
-router.put('/:medicoId', validateMedico, updateMedic)
-router.patch('/:medicoId', validateMedico, disabledMedic)
+router.post('/', createMedic)
+router.put('/:medicoId', updateMedic)
+router.patch('/:medicoId', disabledMedic)
 router.delete('/:medicoId', deleteMedic)
 
 module.exports = router
